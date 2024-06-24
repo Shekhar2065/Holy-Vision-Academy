035a3f7500b119bd026b19c8fadbe79fa058fd29
Shekhar2065 <thapasther101@gmail.com>
2024-06-24 22:24:42 +0545

    changes

--- HVA.js
+++ HVA.js
@@ -61,3 +61,13 @@ function scrolling() {
   imageWrapper.style.left = `-${totalScroll * widthEl}px`
   imageWrapper.style.transition = '.3s'
 }
+
+const toTop = document.querySelector(".to-top");
+
+window.addEventListener("scroll", () => {
+  if (window.pageYOffset > 100) {
+    toTop.classList.add("active");
+  } else {
+    toTop.classList.remove("active");
+  }
+})
